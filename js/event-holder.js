e9c85bb0bb34e96d501b150401e96a9628fa3da2
reecekelly <xXreecekellyXx@gmail.com>
2015-05-13 15:13:53 +0100

    beep issue fixed

--- js/event-holder.js
+++ js/event-holder.js
@@ -5,7 +5,6 @@ function onLoad() {
 }
 			
 window.onload = function() {
-	navigator.notification.beep(2);
 	var headerheight = $(header).height();
 	
 	$("#map-div").css('height', screen.height - (headerheight*2));
