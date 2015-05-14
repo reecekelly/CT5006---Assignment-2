3842464041080513277196723f0e8f753741f843
Reece Kelly <xXreecekellyXx@gmail.com>
2015-05-14 18:41:52 +0100

    removed network stuff

--- js/event-loader.js
+++ js/event-loader.js
@@ -1,42 +1,37 @@
 var eventlink = "http://ct5006-14n.studentsites.glos.ac.uk/php/event-addition.php";
 var eventurl = [];
+
 function loadEvents() {
-	var internet = JSON.stringify(navigator.network.connection.type);
-	
-	if(internet =! "none") {
-		$.ajax({
-			url : eventlink,
-			type : "POST",
-			success: function( data ){ 
-				eventobject = JSON.parse(data);	
 
-				var startpoint = document.getElementById("news");
-						
-				for(var i=0; i<eventobject.length; i++) {
+	$.ajax({
+		url : eventlink,
+		type : "POST",
+		success: function( data ){ 
+			eventobject = JSON.parse(data);	
+			var startpoint = document.getElementById("news");
 					
-					var event = eventobject[i].event_location;
-					eventurl.push(event.event_link);
+			for(var i=0; i<eventobject.length; i++) {
+					
+				var event = eventobject[i].event_location;
+				eventurl.push(event.event_link);
 
-					var newsreel = document.createElement("li");
-					$(newsreel).html('<a href="#" onClick="inAppBrowser('+ i +');" rel="external">' +
-										'<li class="event-holder">' +
-											"<img src='data:image/png;base64," + event.event_picture + "' />"+
-											"<h2>" +
-												"<span>"+event.event_name+"</span>" +
-												"<span class='second'>"+event.event_date+"</span>" +
-											"</h2>" +
-										"</li>" +
-									"</a>");
-							
-					startpoint.appendChild(newsreel);
-				}
-				
-			},
-			error: function( data ){ 
-				console.log("No Event Load"); 
+				var newsreel = document.createElement("li");
+				$(newsreel).html('<a href="#" onClick="inAppBrowser('+ i +');" rel="external">' +
+									'<li class="event-holder">' +
+										"<img src='data:image/png;base64," + event.event_picture + "' />"+
+										"<h2>" +
+											"<span>"+event.event_name+"</span>" +
+											"<span class='second'>"+event.event_date+"</span>" +
+										"</h2>" +
+									"</li>" +
+								"</a>");
+						
+				startpoint.appendChild(newsreel);
 			}
-		});
-	} else {
-		navigator.vibrate(1000);
-	}
+			
+		},
+		error: function( data ){ 
+			console.log("No Event Load"); 
+		}
+	});
 }
